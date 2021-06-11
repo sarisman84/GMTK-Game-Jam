e2871740e4d283c7f621f00e57848565d9cb887b
Spyro <epicdudeness677@gmail.com>
2021-06-11 21:36:25 +0200

    Added Weapon Gameplay and FX. Added some elements in Testing Scene.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -8,14 +8,32 @@ namespace Player
     public class PlayerController : MonoBehaviour
     {
         private Rigidbody _rigidbody;
+        private WeaponController _weaponController;
+
+
+        public Transform model;
         public InputActionAsset inputAsset;
         public float maxMovementSpeed;
         public float accelerationSpeed;
 
         private void Awake()
         {
-            CustomInput.ImportAsset(inputAsset, CustomInput.DirectionalKeys, "DirectionalShot", "Possess");
+            CustomInput.ImportAsset(inputAsset, CustomInput.DirectionalKeys, "DirectionalShot", "Possess", "Fire");
             _rigidbody = GetComponent<Rigidbody>();
+            _weaponController = GetComponent<WeaponController>();
+        }
+
+        private void Update()
+        {
+            if (_weaponController)
+            {
+                _weaponController.Aim(CustomInput.GetDirectionalInput("DirectionalShot"));
+                _weaponController.Shoot(CustomInput.GetButton("Fire"));
+            }
+
+            Vector3 movementDir = _rigidbody.velocity.normalized;
+            model.rotation = Quaternion.LookRotation(movementDir, Vector3.up);
+            
         }
 
         private void FixedUpdate()
